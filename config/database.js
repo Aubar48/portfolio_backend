f0cf2d3907a283ff78e2e129da383a18304255a0
Aubar48 <rociio.fressa@gmail.com>
2025-05-26 12:38:46 -0300

    fix

--- config/database.js
+++ config/database.js
@@ -11,18 +11,6 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    pool: {
-      max: 2,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
   }
 );
 
