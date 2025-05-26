8361a1c9946c891d224a9370c685b6bd56f23631
Aubar48 <rociio.fressa@gmail.com>
2025-05-26 09:47:57 -0300

    env fixed

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || 'localhost',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
